71a875b43aecc86741c6a2bd7582cae9a1e8ff96
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-09 07:31:06 -0500

    Actualizar caché PWA e incluir icono

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='agenda-v3';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase.js','./manifest.json'];
+const CACHE='agenda-v4';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase.js','./manifest.json','./icon.svg'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
