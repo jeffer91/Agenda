7c46e8276ec6dc9e363c0dacbfe11dbdbb5d56f5
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-09 06:26:21 -0500

    Incluir Firebase en la caché PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='agenda-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
+const CACHE='agenda-v3';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase.js','./manifest.json'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
